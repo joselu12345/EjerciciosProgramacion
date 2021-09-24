f555f703b0ea6d20281424faee2786996d760ee1
Jose Luis Ruz Gil <jruzgil23@iesmarquesdecomares.org>
2021-09-24 10:29:00 +0200

    pido 3 numeros

--- src/MiClase.java
+++
@@ -1,9 +0,0 @@
-
-public class MiClase {
-
-	public static void main(String[] args) {
-		System.out.println("Hola mundo");
-
-	}
-
-}
---
+++ src/capitulo01/bloque01/Ejercicio01.java
@@ -0,0 +1,22 @@
+package capitulo01.bloque01;
+
+import javax.swing.JOptionPane;
+
+public class Ejercicio01 {
+
+	public static void main(String[] args) {
+		
+		String str = JOptionPane.showInputDialog("Introduzca un numero entero: "); // Saca en pantalla una ventana y obtiene un string
+		int var1 = Integer.parseInt(str);
+		
+		str = JOptionPane.showInputDialog("Introduzca un número flotante");
+		float var2 = Float.parseFloat(str);
+		
+		str = JOptionPane.showInputDialog("Introduzca un número double");
+		double var3 = Double.parseDouble(str);
+		
+		System.out.println("Numero entero: " + var1 + " - Numero flotante: " + var2 + " - Numero double: " + var3); // Imprime en la consola la palabra introducida
+
+	}
+
+}
