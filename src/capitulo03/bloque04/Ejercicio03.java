393131e7556d6992944da9274ef17a90533d6507
Jose Luis Ruz Gil <jruzgil23@iesmarquesdecomares.org>
2021-10-19 12:58:56 +0200

    bucles sin for desde clase

--- src/capitulo03/bloque04/Ejercicio03.java
+++ src/capitulo03/bloque04/Ejercicio03.java
@@ -4,11 +4,18 @@ public class Ejercicio03 {
 	
 	public static void main (String[] args) {
 		
-		int factor = 5, multiplicador = 0;
+		int factor = 0, multiplicador = 1;
 		
-		do { System.out.println(factor + " x " +  multiplicador  + " = " + (factor * multiplicador));
-			multiplicador++;
-		} while (multiplicador < 15); 
+		do { 
+			factor++;		
+		
+			do { System.out.println(factor + " x " +  multiplicador  + " = " + (factor * multiplicador));
+				multiplicador++;
+				
+			} while (multiplicador <= 10); 
+			  multiplicador = 1;
+		
+		} while ( factor < 15);
 			
       
 	}
--- src/capitulo03/bloque04/Ejercicio04.java
+++ src/capitulo03/bloque04/Ejercicio04.java
@@ -1,5 +1,30 @@
 package capitulo03.bloque04;
 
+import javax.swing.JOptionPane;
+
 public class Ejercicio04 {
+	
+	public static void main (String[] args) {
+		
+		String str;
+		int num, i = (int) Math.pow(2, 30);
+		
+		str = JOptionPane.showInputDialog("Introduzca un numero decimal para saber su equivalencia en binario");
+		num = Integer.parseInt(str);
+		
+		while ( i > 0 ) {
+			
+			if ((num & i) != 0) {
+				System.out.print("1");
+			}
+			else {
+				System.out.print("0");
+			}
+			
+			i/=2;
+		}
+		
+		
+	}
 
 }
