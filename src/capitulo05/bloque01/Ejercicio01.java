d30f11cf783ee2fdbbda9b9cf8dc5d013bfdf249
Jose Luis Ruz Gil <jruzgil23@iesmarquesdecomares.org>
2021-10-26 13:04:20 +0200

    array

---
+++ src/capitulo05/bloque01/Ejercicio01.java
@@ -0,0 +1,43 @@
+package capitulo05.bloque01;
+
+public class Ejercicio01 {
+
+	public static void main(String[] args) {
+
+		int array[] = new int[5];
+
+		int suma = 0, mayor = 0, menor = 0; 
+		float media = 0;
+		
+		System.out.print("el array creado es: \n");
+		
+		
+		for (int i = 0; i < array.length; i++) {
+			array[i] = (int) Math.round(Math.random() * 100);
+
+			System.out.print(array[i] + " ");
+			
+			suma += array[i]; 
+			media = (float) suma/array.length;
+			
+			
+			menor = array[0];
+			
+			if (array[i] > mayor ) {
+				mayor = array[i];
+			}
+			if (array[i] < menor) {
+				menor = array[i];
+			}
+
+		}
+		System.out.println("\nla suma del array es: " + suma);
+		System.out.println("la media del array es: " + media);
+		System.out.println("el mayor es: " + mayor);
+		System.out.println("el menor es: " + menor);	
+		
+		
+
+	}
+
+}
