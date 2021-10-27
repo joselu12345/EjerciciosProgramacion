8cba602bdd6bb14a687c97f8d30495ed6cbec26e
Jose Luis Ruz Gil <jruzgil23@iesmarquesdecomares.org>
2021-10-27 11:29:09 +0200

    array clase

--- src/capitulo05/bloque01/Ejercicio01.java
+++ src/capitulo05/bloque01/Ejercicio01.java
@@ -19,18 +19,24 @@ public class Ejercicio01 {
 			
 			suma += array[i]; 
 			media = (float) suma/array.length;
+		}	
 			
+			menor = mayor = array[0];
 			
-			menor = array[0];
-			
-			if (array[i] > mayor ) {
-				mayor = array[i];
+		 for (int i = 1; i < array.length; i++) {
+				
+				if (array[i] > mayor ) {
+					mayor = array[i];
+				}
+				if (array[i] < menor) {
+					menor = array[i];
+				}
+				
 			}
-			if (array[i] < menor) {
-				menor = array[i];
-			}
+			
+			
 
-		}
+		
 		System.out.println("\nla suma del array es: " + suma);
 		System.out.println("la media del array es: " + media);
 		System.out.println("el mayor es: " + mayor);
