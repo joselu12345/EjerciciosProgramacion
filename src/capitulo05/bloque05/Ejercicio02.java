74f6a7744514eb2edf145f772cfe461adc7a7576
Jose Luis Ruz Gil <jruzgil23@iesmarquesdecomares.org>
2021-11-12 09:39:11 +0100

    arrays y matrices

--- src/capitulo05/bloque05/Ejercicio02.java
+++ src/capitulo05/bloque05/Ejercicio02.java
@@ -1,5 +1,27 @@
 package capitulo05.bloque05;
 
 public class Ejercicio02 {
+	
+	public static void main(String[] args) {
+		
+		int array[] = new int[20];
+		int aprobados = 0, suspensos = 0;
+		
+		for (int i = 0; i < array.length; i++) {
+			array[i] = (int) Math.round(Math.random() * 10);
+			System.out.print(array[i] + " ");
+			
+			if (array[i] >= 5) {
+				aprobados++;
+			}
+			if (array[i] < 5) {
+				suspensos++;
+			}
+		}
+		
+		System.out.println("\nEl porcentaje de aprobados es: " + (aprobados*100)/array.length + "%");
+		System.out.println("\nEl porcentaje de suspensos es: " + (suspensos*100)/array.length + "%");	
+		
+	}
 
 }
--- src/capitulo05/bloque05/Ejercicio03.java
+++ src/capitulo05/bloque05/Ejercicio03.java
@@ -3,8 +3,24 @@ package capitulo05.bloque05;
 public class Ejercicio03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		float array[] = new float[20];
+		int entero;
+		float decimal;
+		int contador = 0;
+		
+		for (int i = 0; i < array.length; i++) {
+			entero = (int) Math.round(Math.random() * 100);
+			decimal = (float) Math.round(Math.random() * 100) / 100;
+			
+			array[i] = entero + decimal;
+			
+			if (decimal <= 0.49) {
+				contador ++;	 
+				System.out.print(array[i] + " - ");
+			}
+		}
+		System.out.println("\nLa cantidad de numeros cuyo decimal esta comprendido entre 0.00 y 0.49 es: " + contador);		
 	}
 
 }
