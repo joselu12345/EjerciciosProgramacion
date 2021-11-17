fe15133ec47696600fc77876055e4b73e2648da9
Jose Luis Ruz Gil <jruzgil23@iesmarquesdecomares.org>
2021-11-17 11:30:05 +0100

    matrices clase

--- src/capitulo05/bloque06/Ejercicio01.java
+++ src/capitulo05/bloque06/Ejercicio01.java
@@ -5,36 +5,26 @@ public class Ejercicio01 {
 	public static void main(String[] args) {
 		
 		int array[] = new int[7];
-		int i = 0;
 		
-		System.out.println("El array creado es:");
+		System.out.println("Los numeros de la loteria son:");
 		
 		
-		for (i = 0; i < array.length; i++) {
+		for (int i = 0; i < array.length; i++) {
 			array[i] = (int) Math.round(Math.random() * 49);
 			
-			
-			
-			System.out.print(array[i] + " ");
-			
-		}
-/*		
-		public static boolean (int [] array, int numero) {
-			
-			boolean repetido = false;
-			for (i = 0; i < array.length; i++) {
-				if (array[i] == numero) {
-					repetido = true;
-				}else if(array[i] != numero) {
-					repetido = false;
+			if (i != 0) {
+				if (i < array.length) {
+					for (int j = i; j > 0; j--) {
+						if (array[i] == array[j - 1]) {
+							array[i] = (int) Math.round(Math.random() * (49 - 1) + 1);
+							j++;
+						}
+					}
 				}
 			}
-			return repetido;
 		}
-*/
-		System.out.println("\n");
 		
-		for (i = 0; i < array.length; i++) {
+		for (int i = 0; i < array.length; i++) {
 			System.out.print(array[i] + " ");
 		}
 	
--- src/capitulo05/bloque06/Ejercicio02.java
+++ src/capitulo05/bloque06/Ejercicio02.java
@@ -5,10 +5,10 @@ public class Ejercicio02 {
 	public static void main(String[] args) {
 		
 		int i, j = 0;
-		char matriz[][] = new char[30][10];
+		char matriz[][] = new char[10][30];
 		
 		for (i = 0; i < matriz.length; i++) {
-            for (j = 0; j < matriz[j].length; j++) {
+            for (j = 0; j < (int) Math.round(Math.random() * 30); j++) {
                 
                 System.out.print("*");
             }
