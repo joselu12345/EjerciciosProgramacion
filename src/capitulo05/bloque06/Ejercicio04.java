9ce53b4e85d9b9c41ff8e27669e6eaf4d973e470
Jose Luis <Jose Luis@DESKTOP-7GV0P99>
2021-11-18 22:49:59 +0100

    matrices casa compis

--- src/capitulo05/bloque06/Ejercicio04.java
+++ src/capitulo05/bloque06/Ejercicio04.java
@@ -4,33 +4,46 @@ public class Ejercicio04 {
 
 	public static void main(String[] args) {
 		
-		int i, j = 0;
-		char matriz[][] = new char[10][20];
-		
-		for (i = 0; i < matriz.length; i++) {
-            for (j = 0; j < matriz[i].length; j++) {
-            	
-            	if (i == 0 || i == matriz.length - 1) {
-            		
-            		System.out.print("*");
-            		
-            	} else {
-            		
-            		if (i == 0 || i == matriz.length - 1) {
-            			System.out.println("*");
-            		}
-            		if (i != 0 && i == matriz.length -1) {
-            			System.out.println(" ");
-            		}
-            	}
-                if (i == matriz.length - 1) {
-                	System.out.println("\n");
-                }
-                	
-            }
-            
-        }
+		char[][] array = new char[20][10];
+		inicializarArray(array);
+		imprimirArray(array);
+	}
 
+	public static void inicializarArray(char[][] matriz) {
+		
+		int fila = (int) Math.round(Math.random() * (18 -1) +1);
+		int columna = (int) Math.round(Math.random() * (8 -1) +1);
+		
+
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[i].length; j++) {				
+				
+				if (i == 0 || i == matriz.length - 1) {
+					matriz[i][j] = '*';
+					
+				} else if (j == 0 || j == matriz[0].length - 1) {
+					matriz[i][j] = '*';
+					
+				} else if (i == fila && j == columna) {
+					matriz[i][j] = '0';
+					
+				} else {
+					matriz[i][j] = ' ';
+					
+				} 
+			}
+		}	
+	}
+	
+	public static void imprimirArray(char[][] array) {
+		
+		for(char [] i : array) {
+			for(char j : i) {
+				System.out.print(j + "  ");
+			}
+			System.out.println();
+		}
+		
 	}
 
 }
--- src/capitulo05/bloque06/Ejercicio05.java
+++ src/capitulo05/bloque06/Ejercicio05.java
@@ -1,10 +1,44 @@
 package capitulo05.bloque06;
 
 public class Ejercicio05 {
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	public static void main(String[] args) {
+
+		char[][] array = new char[20][10];
+		inicializarArray(array);
+		imprimirArray(array);
+	}
+
+	public static void inicializarArray(char[][] matriz) {
+
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[i].length; j++) {
+				int num = (int) Math.round(Math.random() * 1);
+				char relleno = (char) (num + '0');
+				
+				if (i == 0 || i == matriz.length - 1) {
+					matriz[i][j] = '*';
+					
+				} else if (j == 0 || j == matriz[0].length - 1) {
+					matriz[i][j] = '*';
+					
+				} else {
+					matriz[i][j] = relleno;
+				}
+
+			}
+		}
+	}
+	
+	public static void imprimirArray(char[][] array) {
+		
+		for(char [] i : array) {
+			for(char j : i) {
+				System.out.print(j + "  ");
+			}
+			System.out.println();
+		}
+		
 	}
 
 }
