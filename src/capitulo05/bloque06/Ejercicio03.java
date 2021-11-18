b6e76f8e5667c5e91829e85b19f60d13afdd8906
Jose Luis <Jose Luis@DESKTOP-7GV0P99>
2021-11-18 03:03:25 +0100

    matrices casa

--- src/capitulo05/bloque06/Ejercicio03.java
+++ src/capitulo05/bloque06/Ejercicio03.java
@@ -4,7 +4,19 @@ public class Ejercicio03 {
 
 	public static void main(String[] args) {
 		
+		int num = 0, suma = 0, contador = 0;
+		float media = 0;
 		
+		do {
+			
+			num = (int) Math.round(Math.random() * (1000 - 1) + 1);
+			suma += num;
+			contador++;
+			media = (float) suma / contador;
+			
+		} while (media < 499.5f || media > 500.5f);
+		
+		System.out.println("El total de numeros aleatorios generados es: " + contador);
 
 	}
 
--- src/capitulo05/bloque06/Ejercicio04.java
+++ src/capitulo05/bloque06/Ejercicio04.java
@@ -3,7 +3,33 @@ package capitulo05.bloque06;
 public class Ejercicio04 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int i, j = 0;
+		char matriz[][] = new char[10][20];
+		
+		for (i = 0; i < matriz.length; i++) {
+            for (j = 0; j < matriz[i].length; j++) {
+            	
+            	if (i == 0 || i == matriz.length - 1) {
+            		
+            		System.out.print("*");
+            		
+            	} else {
+            		
+            		if (i == 0 || i == matriz.length - 1) {
+            			System.out.println("*");
+            		}
+            		if (i != 0 && i == matriz.length -1) {
+            			System.out.println(" ");
+            		}
+            	}
+                if (i == matriz.length - 1) {
+                	System.out.println("\n");
+                }
+                	
+            }
+            
+        }
 
 	}
 
