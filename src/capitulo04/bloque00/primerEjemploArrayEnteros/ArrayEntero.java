dcbcaec40875df3f6742fc9d97b3647ff155a3af
Jose Luis Ruz Gil <jruzgil23@iesmarquesdecomares.org>
2021-12-01 11:33:27 +0100

    clases

--- src/capitulo04/bloque00/primerEjemploArrayEnteros/ArrayEntero.java
+++ src/capitulo04/bloque00/primerEjemploArrayEnteros/ArrayEntero.java
@@ -2,7 +2,7 @@ package capitulo04.bloque00.primerEjemploArrayEnteros;
 
 public class ArrayEntero {
 	
-	//private ArrayEntero [] Entero = new Entero () ;
+	//aa private ArrayEntero [] Entero = new Entero () ;
 	
 	
 
