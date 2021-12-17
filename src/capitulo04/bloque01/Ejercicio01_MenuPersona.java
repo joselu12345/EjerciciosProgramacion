a7fdfb09f50204a232adeff0b2527a4b993bb785
Jose Luis Ruz Gil <jruzgil23@iesmarquesdecomares.org>
2021-12-17 11:07:47 +0100

    correccion

--- src/capitulo04/bloque01/Ejercicio01_MenuPersona.java
+++ src/capitulo04/bloque01/Ejercicio01_MenuPersona.java
@@ -3,66 +3,70 @@ package capitulo04.bloque01;
 import javax.swing.JOptionPane;
 
 public class Ejercicio01_MenuPersona {
-	
+
 	public static void main(String[] args) {
-		
-		Ejercicio01_Persona arrayPersonas [] = new Ejercicio01_Persona [3];
-		
+
+		Ejercicio01_Persona arrayPersonas[] = new Ejercicio01_Persona[3];
+
 		String str;
 		int var;
-		
-		str = JOptionPane.showInputDialog("Menu Personas" + "\n0 - Salir" + "\n1 - Introducir Datos" + "\n2 - Ver las personas introducidas, con todos sus datos");
-		var = Integer.parseInt(str);
 
-		
-		 
-		switch(var) {
-		
-		case 0:
-			break;
-		
-		case 1:
-			
-			System.out.println("Introduzca los datos: ");
-			
-			for (int i = 0; i < arrayPersonas.length; i++) {
-				if (arrayPersonas[i] == null) {
-					arrayPersonas[i] = new Ejercicio01_Persona();
-					
-					String nombre = JOptionPane.showInputDialog("Nombre: ");					
-					arrayPersonas[i].setNombre(nombre);
-					
-					String apellido = JOptionPane.showInputDialog("Apellido: ");					
-					arrayPersonas[i].setApellido(apellido);
-					
-					String dni = JOptionPane.showInputDialog("dni: ");					
-					arrayPersonas[i].setDni(dni);
-					
-					String direccion = JOptionPane.showInputDialog("direccion: ");					
-					arrayPersonas[i].setDireccion(direccion);
-					
-					String telefono = JOptionPane.showInputDialog("telefono: ");					
-					arrayPersonas[i].setTelefono(telefono);
+		do {
+
+			str = JOptionPane.showInputDialog("Menu Personas" + "\n0 - Salir" + "\n1 - Introducir Datos"
+					+ "\n2 - Ver las personas introducidas, con todos sus datos");
+			var = Integer.parseInt(str);
+
+			switch (var) {
+
+			case 0:
+				break;
+
+			case 1:
+
+				System.out.println("Introduzca los datos: ");
+
+				for (int i = 0; i < arrayPersonas.length; i++) {
+					if (arrayPersonas[i] == null) {
+						arrayPersonas[i] = new Ejercicio01_Persona();
+
+						String nombre = JOptionPane.showInputDialog("Nombre: ");
+						arrayPersonas[i].setNombre(nombre);
+
+						String apellido = JOptionPane.showInputDialog("Apellido: ");
+						arrayPersonas[i].setApellido(apellido);
+
+						String dni = JOptionPane.showInputDialog("dni: ");
+						arrayPersonas[i].setDni(dni);
+
+						String direccion = JOptionPane.showInputDialog("direccion: ");
+						arrayPersonas[i].setDireccion(direccion);
+
+						String telefono = JOptionPane.showInputDialog("telefono: ");
+						arrayPersonas[i].setTelefono(telefono);
+					}
 				}
+
+				break;
+
+			case 2:
+				System.out.println("Mostrar datos: ");
+
+				for (int i = 0; i < arrayPersonas.length; i++) {
+					if (arrayPersonas[i] != null) {
+						System.out.println(arrayPersonas[i].toString());
+					}
+				}
+
+				break;
+
+			default:
+				System.out.println("\nLa variable introducida no es valida");
+
 			}
-					
-			break;
-		
-		case 2:
-			System.out.println("Mostrar datos: ");
-			
-			for (int i = 0 ; i < arrayPersonas.length; i++) {
-				System.out.println(arrayPersonas[i].toString());
-			}
-			
-			break;
-		
-		default:
-			System.out.println("\nLa variable introducida no es valida");
-			
-		}
-		
-		
+
+		} while (var != 0);
+
 	}
 
 }
