4a0267b461b8cea10eaa7ab49d91566e5d6b9152
Jose Luis Ruz Gil <jruzgil23@iesmarquesdecomares.org>
2022-01-27 14:48:09 +0100

    ya funciona

--- src/capitulo04/bloque02Herencia/ventanaConCanvas/Cuadro.java
+++ src/capitulo04/bloque02Herencia/ventanaConCanvas/Cuadro.java
@@ -2,6 +2,7 @@ package capitulo04.bloque02Herencia.ventanaConCanvas;
 
 import javax.swing.JFrame;
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.GraphicsConfiguration;
 
 public class Cuadro {
@@ -15,7 +16,7 @@ public class Cuadro {
 		MiCanvas dibujo = new MiCanvas();
 		dibujo.setBackground(Color.WHITE);
 		
-		Cuadro.getContentPane().add(dibujo, borderLayout.CENTER);
+		Cuadro.getContentPane().add(dibujo, BorderLayout.CENTER);
 		Cuadro.setVisible(true);
 		
 	}
