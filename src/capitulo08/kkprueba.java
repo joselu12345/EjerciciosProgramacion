fb7dc14a88c3a8078dc31abfa03594379d7a28fc
Jose Luis Ruz Gil <jruzgil23@iesmarquesdecomares.org>
2022-03-25 10:27:15 +0100

    wbuilder

---
+++ src/capitulo08/kkprueba.java
@@ -0,0 +1,43 @@
+package capitulo08;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class kkprueba {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					kkprueba window = new kkprueba();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public kkprueba() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
