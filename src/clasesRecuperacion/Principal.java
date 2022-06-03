48cc4bd72bddcda568630b96e1a00b65665c7fe4
Jose Luis Ruz Gil <jruzgil23@iesmarquesdecomares.org>
2022-06-03 13:57:08 +0200

    recupitando

--- src/clasesRecuperacion/Principal.java
+++ src/clasesRecuperacion/Principal.java
@@ -6,11 +6,20 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import clasesRecuperacion.controladores.controladorContrato;
+import clasesRecuperacion.modelo.Contrato;
+
 import java.awt.GridBagLayout;
 import javax.swing.JLabel;
 import java.awt.GridBagConstraints;
 import javax.swing.JTextField;
 import java.awt.Insets;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.awt.event.ActionEvent;
 
 public class Principal extends JFrame {
 
@@ -21,6 +30,12 @@ public class Principal extends JFrame {
 	private JTextField jtfLimite;
 	private JTextField jtfIdTipoContrato;
 	private JTextField jtdIdUsuario;
+	private JPanel panel;
+	private JButton btnPrimero;
+	private JButton btnAnterior;
+	private JButton btnSiguiente;
+	private JButton btnUltimo;
+	private JButton btnModificar;
 
 	/**
 	 * Launch the application.
@@ -49,9 +64,9 @@ public class Principal extends JFrame {
 		setContentPane(contentPane);
 		GridBagLayout gbl_contentPane = new GridBagLayout();
 		gbl_contentPane.columnWidths = new int[]{0, 0, 0};
-		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0};
-		gbl_contentPane.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
-		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_contentPane.columnWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
 		contentPane.setLayout(gbl_contentPane);
 		
 		JLabel lblNewLabel = new JLabel("id");
@@ -69,6 +84,10 @@ public class Principal extends JFrame {
 		gbc_jtfID.gridx = 1;
 		gbc_jtfID.gridy = 0;
 		contentPane.add(jtfID, gbc_jtfID);
+		
+		//getContentPane().add(jtfID, gbc_jtfID);
+		//frmAltaDeUsuario.getContentPane().add(jtfEmail, gbc_jtfEmail);
+		
 		jtfID.setColumns(10);
 		
 		JLabel lblNewLabel_1 = new JLabel("descripcion");
@@ -142,18 +161,210 @@ public class Principal extends JFrame {
 		JLabel lblNewLabel_5 = new JLabel("id usuario");
 		GridBagConstraints gbc_lblNewLabel_5 = new GridBagConstraints();
 		gbc_lblNewLabel_5.anchor = GridBagConstraints.EAST;
-		gbc_lblNewLabel_5.insets = new Insets(0, 0, 0, 5);
+		gbc_lblNewLabel_5.insets = new Insets(0, 0, 5, 5);
 		gbc_lblNewLabel_5.gridx = 0;
 		gbc_lblNewLabel_5.gridy = 5;
 		contentPane.add(lblNewLabel_5, gbc_lblNewLabel_5);
 		
 		jtdIdUsuario = new JTextField();
 		GridBagConstraints gbc_jtdIdUsuario = new GridBagConstraints();
+		gbc_jtdIdUsuario.insets = new Insets(0, 0, 5, 0);
 		gbc_jtdIdUsuario.fill = GridBagConstraints.HORIZONTAL;
 		gbc_jtdIdUsuario.gridx = 1;
 		gbc_jtdIdUsuario.gridy = 5;
 		contentPane.add(jtdIdUsuario, gbc_jtdIdUsuario);
 		jtdIdUsuario.setColumns(10);
+		
+		panel = new JPanel();
+		GridBagConstraints gbc_panel = new GridBagConstraints();
+		gbc_panel.gridwidth = 2;
+		gbc_panel.insets = new Insets(0, 0, 0, 5);
+		gbc_panel.fill = GridBagConstraints.BOTH;
+		gbc_panel.gridx = 0;
+		gbc_panel.gridy = 6;
+		contentPane.add(panel, gbc_panel);
+		
+		btnPrimero = new JButton("primero");
+		btnPrimero.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				mostrarContrato();
+				compruebaEstadoBotones();
+			}
+		});
+		panel.add(btnPrimero);
+		
+		btnAnterior = new JButton("anterior");
+		btnAnterior.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				mostrarContratoAnterior();
+				compruebaEstadoBotones();
+			}
+		});
+		panel.add(btnAnterior);
+		
+		btnSiguiente = new JButton("siguiente");
+		btnSiguiente.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				mostrarContratoSiguiente();
+				compruebaEstadoBotones();
+			}
+		});
+		panel.add(btnSiguiente);
+		
+		btnUltimo = new JButton("ultimo");
+		btnUltimo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				mostrarUltimoContrato();
+				compruebaEstadoBotones();
+			}
+		});
+		panel.add(btnUltimo);
+		
+		btnModificar = new JButton("modificar");
+		btnModificar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				modificarContrato();
+			}
+		});
+		panel.add(btnModificar);
+		
+		mostrarContrato();
 	}
+	
+	private void mostrarContrato() {
+		
+		Contrato con = controladorContrato.findPrimero();
+		jtfID.setText("" + con.getId());
+		jtfDescripcion.setText("" + con.getDescripcion());
+		jtfSaldo.setText("" + con.getSaldo());
+		jtfLimite.setText("" + con.getLimite());
+		jtfIdTipoContrato.setText("" + con.getIdTipoContrato());
+		jtdIdUsuario.setText("" + con.getIdUsuario());
+		//System.out.println(con.getId() + " " + con.getDescripcion());
+		
+	}
+	
+	private void mostrarContratoAnterior() {
+		
+		Contrato con = controladorContrato.findAnterior(Integer.parseInt(jtfID.getText()));
+		jtfID.setText("" + con.getId());
+		jtfDescripcion.setText("" + con.getDescripcion());
+		jtfSaldo.setText("" + con.getSaldo());
+		jtfLimite.setText("" + con.getLimite());
+		jtfIdTipoContrato.setText("" + con.getIdTipoContrato());
+		jtdIdUsuario.setText("" + con.getIdUsuario());
+		
+	}
+	
+	private void mostrarContratoSiguiente() {
+		
+		Contrato con = controladorContrato.findSiguiente(Integer.parseInt(jtfID.getText()));
+		jtfID.setText("" + con.getId());
+		jtfDescripcion.setText("" + con.getDescripcion());
+		jtfSaldo.setText("" + con.getSaldo());
+		jtfLimite.setText("" + con.getLimite());
+		jtfIdTipoContrato.setText("" + con.getIdTipoContrato());
+		jtdIdUsuario.setText("" + con.getIdUsuario());
+		
+	}
+	
+	private void mostrarUltimoContrato() {
+		
+		Contrato con = controladorContrato.findUltimo();
+		jtfID.setText("" + con.getId());
+		jtfDescripcion.setText("" + con.getDescripcion());
+		jtfSaldo.setText("" + con.getSaldo());
+		jtfLimite.setText("" + con.getLimite());
+		jtfIdTipoContrato.setText("" + con.getIdTipoContrato());
+		jtdIdUsuario.setText("" + con.getIdUsuario());
+		
+	}
+	
+	private void compruebaEstadoBotones() {
+		
+		if (controladorContrato.findSiguiente(Integer.parseInt(jtfID.getText())) == null) {
+			
+			btnSiguiente.setEnabled(false);
+			btnUltimo.setEnabled(false);
+			
+		}else {
+			btnSiguiente.setEnabled(true);
+			btnUltimo.setEnabled(true);
+		}
+		
+		if (controladorContrato.findAnterior(Integer.parseInt(jtfID.getText())) == null) {
+			
+			btnAnterior.setEnabled(false);
+			btnPrimero.setEnabled(false);
+			
+		}else {
+			btnAnterior.setEnabled(true);
+			btnPrimero.setEnabled(true);
+		}		
+	}
+	
+//	private void modificarContrato() {
+//		
+//		Contrato con = new Contrato();
+//		
+//		jtfID.getText();		
+//	}
+	
+	public static int guardarContrato(Contrato con) {
+		if (con.getId() == 0) {
+			return nuevoContrato(con);
+		}
+		else {
+			return modificarContrato(con);
+		}
+	}
+	
+	public static int modificarContrato(Contrato con) {		
+		int registrosAfectados = 0;
+		try {
+			Statement s = ConnectionManager.getConexion().createStatement();
+			
+			registrosAfectados = s.executeUpdate(
+					"update contrato set descripcion='" + con.getDescripcion() + "', saldo='" + con.getSaldo() + "', limite='" + con.getLimite() + "', idTipoContrato='" + con.getIdTipoContrato() + "', idUsuario='" + con.getIdUsuario() 
+					+ "' " + " where id=" + con.getId());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return registrosAfectados;		
+	}
+	
+	public static int eliminarContrato (int idEliminacion) {		
+		int registrosAfectados = 0;
+		try {
+			Statement s = ConnectionManager.getConexion().createStatement();
+			
+			registrosAfectados = s.executeUpdate(
+					"delete from contrato where id=" + idEliminacion);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return registrosAfectados;		
+	}
+
+	public static int nuevoContrato (Contrato con) {
+		int registrosAfectados = 0;
+		try {
+			Statement s = ConnectionManager.getConexion().createStatement();
+			con.setId(siguienteIdEnTabla("contrato"));
+			registrosAfectados = s.executeUpdate(
+						"insert into contrato values (" + con.getId() + ",'" + con.getDescripcion() + "', '" + con.getSaldo() + ",'" + con.getLimite() + " ,'" + con.getIdTipoContrato() + " ,'" + con.getIdUsuario() + "')");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return registrosAfectados;
+	}
+
 
 }
--- src/clasesRecuperacion/controladores/controladorContrato.java
+++ src/clasesRecuperacion/controladores/controladorContrato.java
@@ -43,6 +43,122 @@ public class controladorContrato {
 //		
 //	}
 	
+	public static Contrato findPrimero(){
+	
+	Contrato con = null;
+	
+	try {
+		
+		Statement s = (Statement) ConnectionManager.getConexion().createStatement(); 
+		
+		ResultSet rs = s.executeQuery ("select * from contrato order by id limit 1");
+		
+		while (rs.next()) {
+			
+			con = new Contrato(rs.getInt("id"), rs.getString("descripcion"), rs.getFloat("saldo"), rs.getFloat("limite"), rs.getInt("idTipoContrato"), rs.getInt("idUsuario"));
+			
+		}
+		
+		rs.close();
+		s.close();
+		
+	}catch (SQLException ex) {
+		System.out.println("Error en la ejecución SQL: " + ex.getMessage());
+		ex.printStackTrace();
+	}
+
+	
+	return con;	
+	
+}
+	
+	public static Contrato findAnterior(int id){
+		
+		Contrato con = null;
+		
+		try {
+			
+			Statement s = (Statement) ConnectionManager.getConexion().createStatement(); 
+			
+			ResultSet rs = s.executeQuery ("Select * from contrato where id <" + id +" order by id desc limit 1");
+			
+			while (rs.next()) {
+				
+				con = new Contrato(rs.getInt("id"), rs.getString("descripcion"), rs.getFloat("saldo"), rs.getFloat("limite"), rs.getInt("idTipoContrato"), rs.getInt("idUsuario"));
+				
+			}
+			
+			rs.close();
+			s.close();
+			
+		}catch (SQLException ex) {
+			System.out.println("Error en la ejecución SQL: " + ex.getMessage());
+			ex.printStackTrace();
+		}
+
+		
+		return con;	
+		
+	}
+	
+	public static Contrato findSiguiente(int id){
+		
+		Contrato con = null;
+		
+		try {
+			
+			Statement s = (Statement) ConnectionManager.getConexion().createStatement(); 
+			
+			ResultSet rs = s.executeQuery ("Select * from contrato where id >" + id +" order by id limit 1");
+			
+			while (rs.next()) {
+				
+				con = new Contrato(rs.getInt("id"), rs.getString("descripcion"), rs.getFloat("saldo"), rs.getFloat("limite"), rs.getInt("idTipoContrato"), rs.getInt("idUsuario"));
+				
+			}
+			
+			rs.close();
+			s.close();
+			
+		}catch (SQLException ex) {
+			System.out.println("Error en la ejecución SQL: " + ex.getMessage());
+			ex.printStackTrace();
+		}
+
+		
+		return con;	
+		
+	}
+	
+	public static Contrato findUltimo(){
+		
+		Contrato con = null;
+		
+		try {
+			
+			Statement s = (Statement) ConnectionManager.getConexion().createStatement(); 
+			
+			ResultSet rs = s.executeQuery ("select * from contrato order by id desc limit 1");
+			
+			while (rs.next()) {
+				
+				con = new Contrato(rs.getInt("id"), rs.getString("descripcion"), rs.getFloat("saldo"), rs.getFloat("limite"), rs.getInt("idTipoContrato"), rs.getInt("idUsuario"));
+				
+			}
+			
+			rs.close();
+			s.close();
+			
+		}catch (SQLException ex) {
+			System.out.println("Error en la ejecución SQL: " + ex.getMessage());
+			ex.printStackTrace();
+		}
+
+		
+		return con;	
+		
+	}
+	
 	public static List<Contrato> findAll(){
 		
 		List<Contrato> lista = new ArrayList<Contrato>();
@@ -79,17 +195,19 @@ public class controladorContrato {
 //		Contrato con = find();
 //		System.out.println(con.getId() + " " + con.getDescripcion());
 		
+		Contrato con = findPrimero();
+		System.out.println(con.getId() + " " + con.getDescripcion());
 		
 		
-		List<Contrato> lista = findAll();
+//		List<Contrato> lista = findAll();
 		
 //		for (Contrato con : lista) {
 //			System.out.println(con);
 //		}
 //		
-		for (int i = 0 ; i < lista.size() ; i++) {
-			System.out.println(lista.get(i));
-		}
+//		for (int i = 0 ; i < lista.size() ; i++) {
+//			System.out.println(lista.get(i));
+//		}
 		
 		
 		
